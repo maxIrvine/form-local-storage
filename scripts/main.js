0f0e4fdd7cda46fdfca10b8a82382fc29ec8d798
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-20 15:46:51 -0400

    adds unfinished storeData function

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
 var $form = $('[data-form="info"]');
-var $name = $('name-field"');
-var $age = $('age-field"');
-var $shirt = $('shirt-field"');
-var $message = $('message-field"');
-var $submit = $('submit"');
+var $name = $('[data-input="name"]');
+var $age = $('age-field');
+var $shirt = $('shirt-field');
+var $message = $('message-field');
+var $submit = $('[data-type="submit"]');
 
+
+function storeData(){
+    $form.on('submit', function(event){
+        console.log("here");
+        $name.on('input', function(event){
+            console.log($name.val());
+        });
+    });
+}
