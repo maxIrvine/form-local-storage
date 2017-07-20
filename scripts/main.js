e5b86cf0d275a58ef6efb48f0999ba55ca0604d5
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-20 15:30:14 -0400

    adds variables using jQuery

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var $form = $('.form');
+var $name = $('name-field"');
+var $age = $('age-field"');
+var $shirt = $('shirt-field"');
+var $message = $('message-field"');
+var $submit = $('submit"');
