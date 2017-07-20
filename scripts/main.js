b0c5ad70b6f3b8387699ca27b49ae66469a44ff2
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-20 17:05:38 -0400

    updates storeData

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,22 @@
 var $form = $('[data-form="info"]');
 var $name = $('[data-input="name"]');
-var $age = $('age-field');
-var $shirt = $('shirt-field');
-var $message = $('message-field');
+var $age = $('[data-input="age"]');
+var $shirtSmall = $('[data-input="shirt-small"]');
+var $shirtMedium = $('[data-input="shirt-medium"]');
+var $shirtLarge = $('[data-input="shirt-large"]');
+var $message = $('[data-input="message"]');
 var $submit = $('[data-type="submit"]');
 
 
 function storeData(){
     $form.on('submit', function(event){
         event.preventDefault();
-        console.log("here");
+        localStorage.setItem('name', $name.val());
+        localStorage.setItem('age', $age.val());
+        localStorage.setItem('shirt small', $shirtSmall.val());
+        localStorage.setItem('shirt medium', $shirtMedium.val());
+        localStorage.setItem('shirt large', $shirtLarge.val());
+        localStorage.setItem('message', $message.val());
     });
 }
 
